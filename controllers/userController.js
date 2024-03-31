1e691c2e292edacedecd9ea1dfded16848c756b4
Weiss Bai <whitesgr03@gmail.com>
2024-03-31 20:53:34 +0800

    Create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+const asyncHandler = require("express-async-handler");
+
+const userCreateGet = async (req, res, next) => {};
+const userCreatePost = async (req, res, next) => {};
+const userAuthGet = async (req, res, next) => {};
+const userAuthPost = async (req, res, next) => {};
+const userLogoutGet = async (req, res, next) => {};
+
+module.exports = {
+	userCreateGet,
+	userCreatePost,
+	userAuthGet,
+	userAuthPost,
+	userLogoutGet,
+};
