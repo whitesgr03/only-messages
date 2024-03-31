a5b16936e97a033162aaa49b97fe8bd4cb5d1a97
Weiss Bai <whitesgr03@gmail.com>
2024-03-31 20:15:46 +0800

    Add helmet module

--- app.js
+++ app.js
@@ -13,6 +13,26 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
+process.env.NODE_ENV === "production" &&
+	app.use(
+		helmet({
+			contentSecurityPolicy: {
+				directives: {
+					imgSrc: [
+						// "storage.googleapis.com",
+						// "ik.imagekit.io",
+						"data:",
+						"blob:",
+					],
+					styleSrc: [
+						"'self'",
+						"fonts.googleapis.com",
+						"necolas.github.io",
+					],
+				},
+			},
+		})
+	);
 app.use(compression());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
