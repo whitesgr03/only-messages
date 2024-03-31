451768be480be3636cd4f567ba8d5b0b3ec84a12
Weiss Bai <whitesgr03@gmail.com>
2024-03-31 20:53:40 +0800

    Create message controller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,30 @@
 const asyncHandler = require("express-async-handler");
 
+const fetch = require("node-fetch");
+
 const index = asyncHandler(async (req, res, next) => {
-	res.render("index");
+	const randomNumber = 5;
+	const response = await fetch(
+		`https://randomuser.me/api/?results=${randomNumber}&inc=email,name,picture`
+	);
+	const data = await response.json();
+
+	res.render("index", { randomUser: data.results });
 });
+
+const messageCreateGet = async (req, res, next) => {};
+const messageCreatePost = async (req, res, next) => {};
+const messageUpdateGet = async (req, res, next) => {};
+const messageUpdatePost = async (req, res, next) => {};
+const messageDeleteGet = async (req, res, next) => {};
+const messageDeletePost = async (req, res, next) => {};
 
 module.exports = {
 	index,
+	messageCreateGet,
+	messageCreatePost,
+	messageUpdateGet,
+	messageUpdatePost,
+	messageDeleteGet,
+	messageDeletePost,
 };
