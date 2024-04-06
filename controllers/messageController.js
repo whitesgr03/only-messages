5bf7c537b6f629e44fca088af12c32245985217f
Weiss Bai <whitesgr03@gmail.com>
2024-04-06 19:53:20 +0800

    Create "messageCreatePost" middleware
    The authenticate middleware will verify whether the user login
    is successful; if so, verify whether all field values sent by
    the user have errors; if so, please call "handleRenderErrorMessage"
    to render the "messageForm" template with error messages; if not,
    then calling "handleAddMessage" to add a new message to the database.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -21,6 +21,71 @@ const messageCreateGet = [
 		res.render("messageForm", { title: "Send a message" });
 	}),
 ];
+const messageCreatePost = [
+	authenticate,
+	asyncHandler(async (req, res, next) => {
+		const validationSchema = {
+			content: {
+				trim: true,
+				isLength: {
+					options: {
+						min: 1,
+						max: 300,
+					},
+					errorMessage: "The content must be less than 300 long.",
+				},
+				escape: true,
+			},
+		};
+
+		await checkSchema(validationSchema, ["body"]).run(req);
+
+		const schemaErrors = validationResult(req);
+
+		const message = {
+			...req.body,
+		};
+
+		const handleAddMessage = async () => {
+			const currentTime = new Date();
+
+			const today = new Date(currentTime).setHours(0, 0, 0, 0);
+			const tomorrow = new Date(currentTime).setDate(
+				currentTime.getDate() + 1
+			);
+
+			const existsMessageOfTheDay = await Message.findOne({
+				createdAt: {
+					$gte: new Date(today),
+					$lt: new Date(tomorrow),
+				},
+			}).exec();
+
+			const newMessage = new Message({
+				...message,
+				user: req.user.id,
+				lastModified: currentTime,
+				createdAt: currentTime,
+				isFirstMessageOfTheDay: !existsMessageOfTheDay,
+			});
+
+			await newMessage.save();
+			res.redirect("/");
+		};
+
+		const handleRenderErrorMessage = () => {
+			res.render("messageForm", {
+				title: "Sign In",
+				message,
+				errors: schemaErrors.mapped(),
+			});
+		};
+
+		schemaErrors.isEmpty()
+			? handleAddMessage()
+			: handleRenderErrorMessage();
+	}),
+];
 const messageUpdateGet = async (req, res, next) => {};
 const messageUpdatePost = async (req, res, next) => {};
 const messageDeleteGet = async (req, res, next) => {};
