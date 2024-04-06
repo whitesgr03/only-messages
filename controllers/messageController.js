0abdf8c09a88a4c3b5b929673e98ec6891b0ccbc
Weiss Bai <whitesgr03@gmail.com>
2024-04-06 21:29:24 +0800

    Add time property to message object in "getFakeMessages".

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -19,6 +19,7 @@ const index = asyncHandler(async (req, res, next) => {
 					imageUrl: data.results[0].picture.medium,
 					name: data.results[0].name.last,
 				},
+				time: '15:33',
 				content:
 					"This is the default message! If you join our members, you can watch all members' messages.",
 			},
@@ -27,6 +28,7 @@ const index = asyncHandler(async (req, res, next) => {
 					imageUrl: data.results[1].picture.medium,
 					name: data.results[1].name.last,
 				},
+				time: '14:30',
 				content: "Create an account to enjoy Only Messages!",
 			},
 		];
