2893dd3c2df0512b5ff239dbb31d99aba4ca58a8
Weiss Bai <whitesgr03@gmail.com>
2024-04-06 21:26:51 +0800

    Add scheme isFirstMessageOfTheDay

--- models/message.js
+++ models/message.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
-	user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-	content: { type: String, required: true },
-	lastModified: { type: Date, required: true },
-	createdAt: { type: Date, immutable: true },
-	expiresAfter: { type: Date, immutable: true },
-});
+const MessageSchema = new Schema(
+	{
+		user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+		content: { type: String, required: true },
+		isFirstMessageOfTheDay: { type: Boolean, immutable: true },
+		lastModified: { type: Date, required: true },
+		createdAt: { type: Date, immutable: true },
+		expiresAfter: { type: Date, immutable: true },
+	},
+);
 
 const MessageModel = mongoose.model("Message", MessageSchema);
 
