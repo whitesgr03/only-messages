a86b9f35803a9f787cccd9a3c7b0fc88ea1041fb
Weiss Bai <whitesgr03@gmail.com>
2024-04-08 11:15:34 +0800

    Change "messageDeleteGet" middleware
    First, call authenticate to check if the user's login is successful.
    Second, call validateId to verify whether the ID format is correct.
    And last, find all messages, then render the "index" template.

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -190,6 +190,21 @@ const messageUpdatePost = [
 			: handleRenderErrorMessage();
 	}),
 ];
+const messageDeleteGet = [
+	authenticate,
+	validateId,
+	asyncHandler(async (req, res, next) => {
+		const messages = await Message.find({})
+			.populate("user")
+			.sort({ createdAt: 1 })
+			.exec();
+
+		res.render("index", {
+			messages,
+			removeId: req.params.id,
+		});
+	}),
+];
 
 module.exports = {
 	index,
