ac5d893e5c6347d441783e07995c9a7bf4689d07
Weiss Bai <whitesgr03@gmail.com>
2024-04-09 20:33:55 +0800

    Remove virtuals

--- models/message.js
+++ models/message.js
@@ -1,34 +1,15 @@
 const mongoose = require("mongoose");
-const { isThisYear, format } = require("date-fns");
 
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema(
-	{
-		user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-		content: { type: String, required: true },
-		lastModified: { type: Date, required: true },
-		isFirstMessageOfTheDay: { type: Boolean, immutable: true },
-		createdAt: { type: Date, immutable: true },
-		expiresAfter: { type: Date, immutable: true },
-	},
-	{
-		virtuals: {
-			date: {
-				get() {
-					return isThisYear(this.createdAt)
-						? format(new Date(this.createdAt), "E, MMMM d")
-						: format(new Date(this.createdAt), "E, MMMM d, y");
-				},
-			},
-			time: {
-				get() {
-					return format(new Date(this.createdAt), "HH:mm");
-				},
-			},
-		},
-	}
-);
+const MessageSchema = new Schema({
+	user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+	content: { type: String, required: true },
+	lastModified: { type: Date, required: true },
+	isFirstMessageOfTheDay: { type: Boolean, immutable: true },
+	createdAt: { type: Date, immutable: true },
+	expiresAfter: { type: Date, immutable: true },
+});
 
 const MessageModel = mongoose.model("Message", MessageSchema);
 
